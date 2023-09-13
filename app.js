dfce895fb058e2642805ec60c2f08d8f4673f81c
Luiz-felipe93 <92874683+Luiz-felipe93@users.noreply.github.com>
2023-09-13 00:36:37 -0300

    adicinando console log

--- app.js
+++ app.js
@@ -1,2 +1,5 @@
 console.log("Rodando o sistema de gerenciamento de jogos")
 console.log("aplicação está se conectado ao banco de dados")
+// mais um console log teste:
+console.log("O paulo fes esse commit sem voce ver na aula!")
+console.log("Enviando para o GITHUB")
